5d2e18b3eb720199d3a136a45756782819a448cc
Victor Arocha <victor.arocha@gmail.com>
2024-12-26 23:11:34 +0000

    firt commit

---
+++ epitaxis/epitaxis/doctype/city/city.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Victor Arocha and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("City", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ epitaxis/epitaxis/doctype/felony/test_felony.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Victor Arocha and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFelony(FrappeTestCase):
+	pass
