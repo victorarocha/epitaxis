05f22cbbe69175148c46ec3df896ce59f65b3470
Victor Arocha <victor.arocha@gmail.com>
2024-12-31 06:38:38 +0000

    some extra doctypes

--- epitaxis/epitaxis/doctype/nationality/nationality.py
+++ epitaxis/epitaxis/doctype/nationality/nationality.py
@@ -5,7 +5,5 @@
 from frappe.model.document import Document
 
 
-class Person(Document):
-	def before_save(self):
-		self.full_name = f"{self.first_name} {self.father_lastname} {self.mother_lastname}"
-		
+class Nationality(Document):
+	pass
